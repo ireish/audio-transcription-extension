38bf0a765e6b2edd6ab13184c18da1728dfeb376
Elon Musk <ireish@hotmail.com>
2025-08-22 20:43:24 -0700

    ops: gcp env config

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,18 @@ import { setupWebSocket } from './websocket-handler'
 
 const app = express()
 
+// Allow CORS from all origins for health/upload endpoints
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204)
+    return
+  }
+  next()
+})
+
 app.use(routes)
 
 const server = http.createServer(app)
